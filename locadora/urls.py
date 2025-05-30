037495cbed266afb47c26f8bc8310c2b98e8c846
Kyutz <thallys.henrique.martins@hotmail.com>
2025-05-30 16:41:51 -0300

    feat: criar página e view para listar carros disponíveis

--- locadora/urls.py
+++ locadora/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import home
+from locadora import views
 
 urlpatterns = [
     path('', home, name='home'),
+    path('carros/', views.carros, name='carros'),
 ]
--- locadora/views.py
+++ locadora/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Carro  # importa o model
 
 def home(request):
     template = loader.get_template('home.html')  
@@ -7,4 +8,14 @@ def home(request):
         "empresa": "AlugaFácil",
         "mensagem": "Bem-vindo ao sistema de aluguel de carros!"
     }
+    return HttpResponse(template.render(context, request))
+
+def carros(request):
+    template = loader.get_template('carros.html')
+    lista_carros = Carro.objects.filter(disponibilidade=True)  # só carros disponíveis
+    
+    context = {
+        "carros": lista_carros,
+    }
+    
     return HttpResponse(template.render(context, request))
