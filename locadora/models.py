3600f3661f1d0194f1b2b7e5ec45575416c0afb5
Kyutz <thallys.henrique.martins@hotmail.com>
2025-05-30 12:05:43 -0300

    feat: adicionar relação entre Cliente e User

--- locadora/models.py
+++ locadora/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Cliente(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     nome = models.CharField(max_length=255)
     email = models.EmailField()
     telefone = models.CharField(max_length=20)
