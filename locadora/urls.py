871ed010c300e4227a0415db9a50ca416c9ca34c
Kyutz <thallys.henrique.martins@hotmail.com>
2025-06-01 14:07:05 -0300

    feat: criar página e view para cadastro de clientes

--- locadora/urls.py
+++ locadora/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import home, listar_locacoes, carros
+from . import views
 
 urlpatterns = [
     path('', home, name='home'),
     path('carros/', carros, name='carros'),
     path('locacoes/', listar_locacoes, name='listar_locacoes'),
+    path('auth/cadastro', views.cadastro, name='cadastro'),
 ]
--- locadora/views.py
+++ locadora/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from .models import Carro, Locacao  # importa o model
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     template = loader.get_template('home.html')  
@@ -24,3 +25,13 @@ def carros(request):
 def listar_locacoes(request):
     locacoes = Locacao.objects.select_related('cliente', 'carro').all()
     return render(request, 'locacoes.html', {'locacoes': locacoes})
+
+def cadastro(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # ou para 'home' se preferir
+    else:
+        form = UserCreationForm()
+    return render(request, 'cadastro.html', {'form': form})
